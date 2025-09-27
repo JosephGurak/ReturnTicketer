5f51771cc97a1f918838e60e0111daba18bc88bf
Joseph Gurak <josephgurak@outlook.com>
2025-09-27 00:10:36 -0400

    [initial commit] initial commit

---
+++ returnticketer/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <main className="container">
+      <h1>Return ticketer</h1>
+      
+      <p>for generating return tickets to place on devices</p>
+
+      
+    </main>
+  );
+}
+
+export default App;
