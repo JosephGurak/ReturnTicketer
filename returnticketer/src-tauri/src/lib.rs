354c606441ad2a53e7d4c0d493da121825d9a0e1
Joseph Gurak <josephgurak@outlook.com>
2025-09-30 23:07:51 -0400

    [update] logic
    added struct and initial impl for Ticket struct. test fn for displaying ticket info to front end works.

--- returnticketer/src-tauri/src/lib.rs
+++ returnticketer/src-tauri/src/lib.rs
@@ -1,14 +1,50 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+
+#[derive(Debug)]
+struct Ticket {
+    ticketId: String,
+    deviceId: String,
+    location: String,
+    issue: String,
 }
+
+impl Ticket {
+    fn new(ticketId: String, deviceId: String, location: String, issue: String) -> Self {
+        Self {
+            ticketId,
+            deviceId,
+            location,
+            issue,
+        }
+    }
+
+    
+
+    /*
+        functions to add new ticket to vector, vector of tickets displayed in a column .
+        vector can have entries removed ,
+        print button to print all the tickets in vector as a docx
+    
+     */
+
+}
+
+
+
+#[tauri::command]
+fn make_ticket(ticketId: &str, deviceId: &str, location: &str, issue: &str) -> String {
+    //println!("TicketId: {}, DeviceID: {}, Location: {}, Issue: {}", ticketId,deviceId,location,issue);
+    format!("TicketId: {}, DeviceID: {}, Location: {}, Issue: {}", ticketId,deviceId,location,issue)
+    
+}
+
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![make_ticket])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- returnticketer/src/App.jsx
+++ returnticketer/src/App.jsx
@@ -3,17 +3,59 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [ticket, setTicket] = useState("");
+  const [ticketId, setticketId] = useState("");
+  const [deviceId, setdeviceId] = useState("");
+  const [location, setlocation] = useState("");
+  const [issue, setissue] = useState("");
   
+  async function makeTicket() {
+    setTicket(await invoke("make_ticket", {ticketId,deviceId,location,issue}));
+  }
 
-  return (
+  const body = [
     <main className="container">
       <h1>Return ticketer</h1>
       
       <p>for generating return tickets to place on devices</p>
-
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          makeTicket();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setticketId(e.currentTarget.value)}
+          placeholder="Enter Ticket Number"
+        />
+        <input
+          id="greet-input"
+          onChange={(e) => setdeviceId(e.currentTarget.value)}
+          placeholder="Enter Device Id"
+        />
+        <input
+          id="greet-input"
+          onChange={(e) => setlocation(e.currentTarget.value)}
+          placeholder="Enter Location"
+        />
+        <input
+          id="greet-input"
+          onChange={(e) => setissue(e.currentTarget.value)}
+          placeholder="Enter Issue"
+        />
+        <button type="submit">Print</button>
+      </form>
+      <p>{ticket}</p>
       
     </main>
+  ]
+  return (
+    <div>
+      {body}
+    </div>
   );
 }
 
